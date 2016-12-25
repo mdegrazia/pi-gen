da12b985b3533ad2cdf55c87f2625f5b4447998f
Mari DeGrazia <arizona4n6@gmail.com>
2016-12-25 14:28:26 -0700

    Update 00-run.sh

--- stage4/02-extras/00-run.sh
+++ stage4/02-extras/00-run.sh
@@ -23,6 +23,11 @@ install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/Documents"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/Documents/BlueJ Projects"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/Documents/Greenfoot Projects"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/Documents/Scratch Projects"
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.config"
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.config/pcmanfm"
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.config/pcmanfm/LXDE-pi"
+
+
 rsync -a --chown=1000:1000 ${ROOTFS_DIR}/usr/share/doc/BlueJ/ "${ROOTFS_DIR}/home/pi/Documents/BlueJ Projects"
 rsync -a --chown=1000:1000 ${ROOTFS_DIR}/usr/share/doc/Greenfoot/ "${ROOTFS_DIR}/home/pi/Documents/Greenfoot Projects"
 rsync -a --chown=1000:1000 ${ROOTFS_DIR}/usr/share/scratch/Projects/Demos/ "${ROOTFS_DIR}/home/pi/Documents/Scratch Projects"
