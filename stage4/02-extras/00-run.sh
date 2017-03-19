fc0e30f1babba05979c95edfa725620ab2e8dc87
Mari DeGrazia <arizona4n6@gmail.com>
2017-03-19 10:05:28 -0700

    added in piWarmer libraries

--- stage4/02-extras/00-run.sh
+++ stage4/02-extras/00-run.sh
@@ -39,6 +39,9 @@ install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.local/share/applications"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.local/share/desktop-directories"
 
 #piWarmer files
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/usr/local/bin/lib"
+install -m 755 files/fona.py 	${ROOTFS_DIR}/usr/local/bin/lib/
+install -m 755 files/relay.py	${ROOTFS_DIR}/usr/local/bin/lib/
 install -m 777 files/piWarmer.config	${ROOTFS_DIR}/home/pi/Desktop/
 install -m 644 files/airplane.jpg	${ROOTFS_DIR}/usr/share/pixel-wallpaper/
 install -m 644 files/airplane_license.txt	${ROOTFS_DIR}/usr/share/pixel-wallpaper/
